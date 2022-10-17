b37ecfd8b7473d2bf9fe1eb8938a6ba2952cc22e
tommydang1 <tommydange@gmail.com>
2022-10-16 19:49:00 -0700

    Adding Arrays & Hashing LC Easy solutions

---
+++ Arrays & Hashing/valid_anagram.cpp
@@ -0,0 +1,28 @@
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> map;
+
+        for (char ch : s) {
+            map[ch]++;
+        }
+
+        for (char ch : t) {
+            if (map[ch] == 0) {
+                return false;
+            }
+            map[ch]--;
+        }
+
+        for (auto it : map) {
+            if (it.second) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
